74161209cd0df1b0b3e501fe85fd2bceabd6d52b
Katya <www@mail.ru>
2022-12-05 14:05:05 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Task 2
+Console.WriteLine("Insert three numbers:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("The most number is:" + max);
+
+
